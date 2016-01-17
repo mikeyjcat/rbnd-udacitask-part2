b2a2786722688f89c0c3601774f97b5fb785644f
mikeyjcat <mike@hewitson.co.za>
2016-01-17 08:02:29 +0200

    Moved date formatting into Listable, and cleaned up style errors

--- lib/listable.rb
+++ lib/listable.rb
@@ -10,4 +10,12 @@ module Listable
     value = '' if !priority
     return value
   end
+
+  def format_date(start_date, end_date = nil)
+    dates = start_date.strftime('%D') if start_date
+    dates << ' -- ' + end_date.strftime('%D') if end_date
+    dates = 'N/A' if !dates
+    return dates
+  end
+
 end
